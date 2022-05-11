d992a1fba0d2703a4af7bafbf35ac65803b63ffa
Austin Stauffer <austin.lee.stauffer@gmail.com>
2022-05-11 11:11:03 -0400

    initial commit

---
+++ mySite.js/mySite.js
@@ -0,0 +1,7 @@
+// When the user clicks on <div>, open the popup
+    function openForm() {
+        document.getElementById("popupForm").style.display = "block";
+      }
+    function closeForm() {
+        document.getElementById("popupForm").style.display = "none";
+      }
